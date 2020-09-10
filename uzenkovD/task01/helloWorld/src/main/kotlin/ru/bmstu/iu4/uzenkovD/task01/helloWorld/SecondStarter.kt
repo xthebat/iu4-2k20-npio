4b76237e8a96e6b54fb13846881178ac340972f0
Chpeck <uzen.demid@gmail.com>
2020-09-10 16:13:23 +0300

    task01 helloWorld done
    helloWorld done
    won in the battle with gradle

---
+++ uzenkovD/task01/helloWorld/src/main/kotlin/ru/bmstu/iu4/uzenkovD/task01/helloWorld/SecondStarter.kt
@@ -0,0 +1,9 @@
+package ru.bmstu.iu4.uzenkovD.task01.helloWorld
+
+object SecondStarter {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Second entry point with another build config...")
+        print("println create new line, print doesnt...")
+    }
+}
---
+++ uzenkovD/task01/helloWorld/src/main/kotlin/ru/bmstu/iu4/uzenkovD/task01/helloWorld/Starter.kt
@@ -0,0 +1,8 @@
+package ru.bmstu.iu4.uzenkovD.task01.helloWorld
+
+object Starter {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("LOL its alive, gradle so funny killer tool")
+    }
+}
