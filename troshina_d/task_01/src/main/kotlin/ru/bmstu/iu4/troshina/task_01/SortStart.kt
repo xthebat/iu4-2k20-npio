be934b562cc558f5b373b8378332fcdfcc8906aa
troshina22 <d.troshina222@gmail.com>
2020-09-10 13:04:07 +0300

    changed var->val

--- troshina_d/task_01/src/main/kotlin/ru/bmstu/iu4/troshina/task_01/SortStart.kt
+++ troshina_d/task_01/src/main/kotlin/ru/bmstu/iu4/troshina/task_01/SortStart.kt
@@ -4,9 +4,8 @@ object SortStart {
     @JvmStatic
     fun main(args: Array<String>) {
         println("Введите числа, для окончания заполнения введите +")
-        var myArray = makeArray()
+        val myArray = makeArray()
         println("Вы ввели следующий массив: " + myArray.contentToString())
-        myArray = qSort(myArray, 0, myArray.lastIndex)
-        println("Отсортированный массив: " + myArray.contentToString())
+        println("Отсортированный массив: " + qSort(myArray, 0, myArray.lastIndex).contentToString())
     }
 }
