9a9c0834c1d2d985b9a9497b2fbb4aa2c949dae3
Chpeck <uzen.demid@gmail.com>
2020-09-11 17:37:14 +0300

    task01 done
    helloWorld + bubbleSort first ugly revision

--- uzenkovD/task01/helloWorld/src/main/kotlin/ru/bmstu/iu4/uzenkovD/task01/helloWorld/SecondStarter.kt
+++
@@ -1,9 +0,0 @@
-package ru.bmstu.iu4.uzenkovD.task01.helloWorld
-
-object SecondStarter {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println("Second entry point with another build config...")
-        print("println create new line, print doesnt...")
-    }
-}
--- uzenkovD/task01/helloWorld/src/main/kotlin/ru/bmstu/iu4/uzenkovD/task01/helloWorld/Starter.kt
+++
@@ -1,8 +0,0 @@
-package ru.bmstu.iu4.uzenkovD.task01.helloWorld
-
-object Starter {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        print("LOL its alive, gradle so funny killer tool")
-    }
-}
