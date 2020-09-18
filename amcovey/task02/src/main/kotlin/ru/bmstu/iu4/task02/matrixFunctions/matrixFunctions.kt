fdc394fd294415853f0ada716b4db3f14187b898
yevocama <alexandermacovey@gmail.com>
2020-09-18 12:45:16 +0300

    add fixes

--- amcovey/task02/src/main/kotlin/ru/bmstu/iu4/task02/matrixFunctions/matrixFunctions.kt
+++ amcovey/task02/src/main/kotlin/ru/bmstu/iu4/task02/matrixFunctions/matrixFunctions.kt
@@ -4,45 +4,45 @@ import ru.bmstu.iu4.task02.vectorFunctions.Vector
 import ru.bmstu.iu4.task02.vectorFunctions.*
 
 typealias Matrix2D = List<Vector>
+
 /* Adding two matrix */
-operator fun Matrix2D.plus(x: Matrix2D): Matrix2D{
-    if ((this.size != x.size) || (this[0].size != x[0].size)) throw  Exception("Vectors size must be equal")
+operator fun Matrix2D.plus(x: Matrix2D): Matrix2D {
+    require((size == x.size) || (this[0].size == x[0].size)) { "Matrix size must be equal" }
     val result = mutableListOf<Vector>()
-    var tmp: Vector
-    for (i in indices){
-        tmp = this[i] + x[i]
+    for (i in indices) {
+        var tmp: Vector = this[i] + x[i]
         result.add(tmp)
     }
     return result
 }
+
 /* Subtracting one matrix from another */
-operator fun Matrix2D.minus(x: Matrix2D): Matrix2D{
-    if ((this.size != x.size) || (this[0].size != x[0].size)) throw  Exception("Vectors size must be equal")
+operator fun Matrix2D.minus(x: Matrix2D): Matrix2D {
+    require((size == x.size) || (this[0].size == x[0].size)) { "Matrix size must be equal" }
     val result = mutableListOf<Vector>()
-    var tmp: Vector
-    for (i in indices){
-        tmp = this[i] - x[i]
+    for (i in indices) {
+        var tmp: Vector = this[i] - x[i]
         result.add(tmp)
     }
     return result
 }
+
 /* Multiplication of two matrices */
-operator fun Matrix2D.times(x: Matrix2D): Matrix2D{
-    if (this[0].size != x.size)
-        throw  Exception("Count of rows in matrix A should be equal columns in matrix B")
+operator fun Matrix2D.times(x: Matrix2D): Matrix2D {
+    require(this[0].size == x.size) { "Count of rows in matrix A should be equal columns in matrix B" }
     val result = mutableListOf<Vector>()
     val transMatrix = mutableListOf<Vector>()
     val tmpRow = mutableListOf<Float>()
-    for (i in x[0].indices){
-        for (j in x.indices){
+    for (i in x[0].indices) {
+        for (j in x.indices) {
             tmpRow.add(x[j][i])
         }
         transMatrix.add(tmpRow.toMutableList())
         tmpRow.clear()
     }
 
-    for (i in indices){
-        for (j in transMatrix.indices){
+    for (i in indices) {
+        for (j in transMatrix.indices) {
             tmpRow.add(this[i] dot transMatrix[j])
         }
         result.add(tmpRow.toMutableList())
@@ -50,12 +50,13 @@ operator fun Matrix2D.times(x: Matrix2D): Matrix2D{
     }
     return result
 }
+
 /* Transpose Matrix */
-fun Matrix2D.transpose(): Matrix2D{
+fun Matrix2D.transpose(): Matrix2D {
     val transMatrix = mutableListOf<Vector>()
-    val tmpRow = mutableListOf<Float>()
-    for (i in this[0].indices){
-        for (j in this.indices){
+    for (i in this[0].indices) {
+        val tmpRow = mutableListOf<Float>()
+        for (j in this.indices) {
             tmpRow.add(this[j][i])
         }
         transMatrix.add(tmpRow.toMutableList())
@@ -63,11 +64,14 @@ fun Matrix2D.transpose(): Matrix2D{
     }
     return transMatrix
 }
+
 /* Matrix cross Vector */
-infix fun Matrix2D.crossVector(x: Vector): Matrix2D{
+infix fun Matrix2D.cross(x: Vector): Matrix2D {
+    require(this[0].size == x.size)
+    { "The number of columns in the matrix must be equal to the number of elements in the vector" }
     val result = mutableListOf<Vector>()
-    val tmpRow = mutableListOf<Float>()
-    for (i in indices){
+    for (i in indices) {
+        val tmpRow = mutableListOf<Float>()
         tmpRow.add(this[i] dot x)
         result.add(tmpRow.toMutableList())
         tmpRow.clear()
--- amcovey/task02/src/test/kotlin/ru/bmstu/iu4/task02/OperationFunctionsKtTest.kt
+++ amcovey/task02/src/test/kotlin/ru/bmstu/iu4/task02/OperationFunctionsKtTest.kt
@@ -14,6 +14,7 @@ internal class OperationFunctionsKtTest {
         val result: Vector = listOf(3F, 3F, 1F)
         assertEquals(result, a + b)
     }
+
     /* I know that a warning is shown here, but I don't know how to test it otherwise plusAssign operator */
     @Test
     fun vectorPlusAssignEqualTest() {
@@ -31,6 +32,7 @@ internal class OperationFunctionsKtTest {
         val result: Vector = listOf(8F, 0F, 5F)
         assertEquals(result, a - b)
     }
+
     /* I know that a warning is shown here, but I don't know how to test it otherwise minusAssign operator */
     @Test
     fun vectorMinusAssignEqualTest() {
@@ -58,53 +60,65 @@ internal class OperationFunctionsKtTest {
     }
 
     @Test
-    fun matrix2DPlusEqualTest(){
-        val a: Matrix2D = listOf(listOf(1F, 2F, 3F),
-                                           listOf(5F, 6F, 7F),
-                                           listOf(10F, 15F, 20F))
-        val b: Matrix2D = listOf(listOf(0F, 5F, 13F),
-                                           listOf(6F, 1F, 10F),
-                                           listOf(1F, 11F, 50F))
-        val result: Matrix2D = listOf(listOf(1F, 7F, 16F),
-                                                listOf(11F, 7F, 17F),
-                                                listOf(11F, 26F, 70F))
+    fun matrix2DPlusEqualTest() {
+        val a: Matrix2D = listOf(
+            listOf(1F, 2F, 3F),
+            listOf(5F, 6F, 7F),
+            listOf(10F, 15F, 20F)
+        )
+        val b: Matrix2D = listOf(
+            listOf(0F, 5F, 13F),
+            listOf(6F, 1F, 10F),
+            listOf(1F, 11F, 50F)
+        )
+        val result: Matrix2D = listOf(
+            listOf(1F, 7F, 16F),
+            listOf(11F, 7F, 17F),
+            listOf(11F, 26F, 70F)
+        )
         assertEquals(result, (a + b))
     }
 
     @Test
-    fun matrix2DMinusEqualTest(){
-        val a: Matrix2D = listOf(listOf(1F, 2F, 3F),
+    fun matrix2DMinusEqualTest() {
+        val a: Matrix2D = listOf(
+            listOf(1F, 2F, 3F),
             listOf(5F, 6F, 7F),
-            listOf(10F, 15F, 20F))
-        val b: Matrix2D = listOf(listOf(0F, 5F, 13F),
+            listOf(10F, 15F, 20F)
+        )
+        val b: Matrix2D = listOf(
+            listOf(0F, 5F, 13F),
             listOf(6F, 1F, 10F),
-            listOf(1F, 11F, 50F))
-        val result: Matrix2D = listOf(listOf(1F, -3F, -10F),
+            listOf(1F, 11F, 50F)
+        )
+        val result: Matrix2D = listOf(
+            listOf(1F, -3F, -10F),
             listOf(-1F, 5F, -3F),
-            listOf(9F, 4F, -30F))
+            listOf(9F, 4F, -30F)
+        )
         assertEquals(result, (a - b))
     }
 
     @Test
-    fun matrix2DMultiplicationEqualTest(){
+    fun matrix2DMultiplicationEqualTest() {
         val a: Matrix2D = listOf(listOf(-1F, 1F), listOf(2F, 0F), listOf(0F, 3F))
         val b: Matrix2D = listOf(listOf(3F, 1F, 2F), listOf(0F, -1F, 4F))
         val result: Matrix2D = listOf(listOf(-3F, -2F, 2F), listOf(6F, 2F, 4F), listOf(0F, -3F, 12F))
         assertEquals(result, (a * b))
     }
-    
+
     @Test
-    fun matrix2DTransposeEqualTest(){
+    fun matrix2DTransposeEqualTest() {
         val a: Matrix2D = listOf(listOf(4F, 2F, 5F), listOf(9F, 0F, 6F))
         val result: Matrix2D = listOf(listOf(4F, 9F), listOf(2F, 0F), listOf(5F, 6F))
         assertEquals(result, a.transpose())
     }
 
     @Test
-    fun matrix2DCrossVectorEqualTest(){
+    fun matrix2DCrossVectorEqualTest() {
         val a: Matrix2D = listOf(listOf(2F, 4F, 0F), listOf(-2F, 1F, 3F), listOf(-1F, 0F, 1F))
         val b: Vector = listOf(1F, 2F, -1F)
         val result = listOf(listOf(10F), listOf(-3F), listOf(-2F))
-        assertEquals(result, a crossVector b)
+        assertEquals(result, a cross b)
     }
 }
