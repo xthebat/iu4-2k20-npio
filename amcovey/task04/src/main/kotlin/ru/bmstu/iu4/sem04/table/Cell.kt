049a346fd47b9d830aeb6a940b399f54378fc066
yevocama <alexandermacovey@gmail.com>
2020-10-06 20:21:17 +0300

    Macovey Task04

--- amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Cell.kt
+++ amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Cell.kt
@@ -9,13 +9,32 @@ data class Cell(
     var width: Int,
     var height: Int,
 ) {
-    var left = true
-    var right = true
+    var left = false
+    var right = false
     var top = true
     var bottom = true
 
+    var centerAlign = true
+
     var verticalEdge = '-'
     var horizontalEdge = '|'
+    private var spaces = "%${width}s".format(" ")
+
+    private fun MutableList<String>.popFirstElement(): String{
+        return if (!isEmpty()){
+            val item = first()
+            remove(item)
+            item
+        }
+        else {
+            spaces
+        }
+    }
+
+    fun adjustCell(newWidth: Int?){
+        width = newWidth!!
+        spaces = "%${width}s".format(" ")
+    }
 
     /**
      *      width
@@ -26,9 +45,21 @@ data class Cell(
      *  |------------|
      */
     fun build(): String {
-        val spaces = "%${width}s".format(" ")
+//        val spaces = "%${width}s".format(" ")
         val horizontal = buildString { repeat(width) { append(verticalEdge) } }
-        val line = text.stretch(width)
+
+        val textIntoCell = mutableListOf<String>()
+        if (text.length >= width){
+            val substringIndices = if (text.length % width == 0) (text.length / width - 1)
+            else (text.length / width)
+            textIntoCell.add(text.stretch(width))
+            collect(substringIndices){
+                textIntoCell.add(text
+                    .substring(textIntoCell[0].length * (it + 1))
+                    .stretch(textIntoCell[0].length))
+            }
+        }
+        else textIntoCell.add(text)
 
         var totalHeight = height
         if (top) totalHeight++
@@ -36,15 +67,21 @@ data class Cell(
 
         val center = totalHeight / 2
 
-        // [0, 1, 2, 3 ... totalHeight - 1] -> ["|-------------|",  "|             |", ..... "....]
-        // it = 0 -> |-------------|
-        // it = 1 -> |             |
-        // .....
-
         return collect(totalHeight) {
             val internal = when (it) {
                 0 -> if (top) horizontal else spaces
-                center -> line
+                in center..(center + textIntoCell.size) -> {
+                    val line = textIntoCell.popFirstElement()
+                    if (centerAlign){
+                        buildString {
+                            repeat((width - line.length) / 2 ) { append(" ")}
+                            append(line)
+                            repeat((width - line.length + 1) / 2 ) { append(" ")}
+                        }
+                    }
+                    else
+                        line.padEnd(width, ' ')
+                }
                 totalHeight - 1 -> if (bottom) horizontal else spaces
                 else -> spaces
             }
--- amcovey/task04/src/test/kotlin/ru/bmstu/iu4/sem04/table/Tests.kt
+++ amcovey/task04/src/test/kotlin/ru/bmstu/iu4/sem04/table/Tests.kt
@@ -2,6 +2,7 @@ package ru.bmstu.iu4.sem04.table
 
 import org.junit.Test
 import ru.inforion.lab403.common.logging.logger
+import kotlin.test.assertEquals
 
 
 internal class Tests {
@@ -11,36 +12,223 @@ internal class Tests {
 
     @Test
     fun buildCellTest() {
-//        val cell = Cell("hello world", 15, 5)
-//        cell.top = false
-//        cell.bottom = false
+        val text = Cell("MyCeeeell", 15, 5).apply {
+            top = true
+            bottom = true
+            left = true
+            right = true
+            horizontalEdge = '|'
+            centerAlign = false
+        }
+        val result = """
+                        |---------------|
+                        |               |
+                        |               |
+                        |MyCeeeell      |
+                        |               |
+                        |               |
+                        |---------------|
+        """.trimIndent()
+        val testCell = text.build()
+        log.info { "\n$testCell" }
+        assertEquals(result, testCell)
 
-        val text = Cell("hello world", 15, 5).apply {
-//            top = false
-            bottom = false
-//            left = false
-            right = false
-            width = 20
-            horizontalEdge = '+'
-        }.build()
+        val textCenter = Cell("MyCeeeell", 15, 5).apply {
+            top = true
+            bottom = true
+            left = true
+            right = true
+            horizontalEdge = '|'
+            centerAlign = true
+        }
+        val resultCenter = """
+                        |---------------|
+                        |               |
+                        |               |
+                        |   MyCeeeell   |
+                        |               |
+                        |               |
+                        |---------------|
+        """.trimIndent()
+        val testCellCenter = textCenter.build()
+        log.info { "\n$testCellCenter" }
+        assertEquals(resultCenter, testCellCenter)
 
-        log.info { "\n$text" }  // "\n" + text
+        val textAdjust = Cell("MyCeeeell", 15, 5).apply {
+            top = true
+            bottom = true
+            left = true
+            right = true
+            horizontalEdge = '|'
+            centerAlign = true
+        }
+        val resultAdjust = """
+                        |---------|
+                        |         |
+                        |         |
+                        |MyCeeeell|
+                        |         |
+                        |         |
+                        |---------|
+        """.trimIndent()
+        textAdjust.adjustCell(textAdjust.text.length)
+        val testCellAdjust = textAdjust.build()
+        log.info { "\n$testCellAdjust" }
+        assertEquals(resultAdjust, testCellAdjust)
+
+        val textCarry = Cell("MyCeeeell", 5, 5).apply {
+            top = true
+            bottom = true
+            left = true
+            right = true
+            horizontalEdge = '|'
+            centerAlign = true
+        }
+        val resultCarry = """
+                            |-----|
+                            |     |
+                            |     |
+                            |MyCee|
+                            |eell |
+                            |     |
+                            |-----|
+        """.trimIndent()
+        val testCarry = textCarry.build()
+        log.info { "\n$testCarry" }
+        assertEquals(resultCarry, testCarry)
     }
 
     @Test
     fun buildRowTest() {
-        val text = Row("hello world", "...", "test", width = 15, height = 5).build()
+        val text = Row("first", "second", "third", "12345678901234567890", width = 15, height = 5).apply {
+            rowBottom = true
+            rowTop = true
+            doubleFrames = false
+        }
+        val testRow = text.build()
+        val result = """
+                        |---------------|---------------|---------------|---------------|
+                        |               |               |               |               |
+                        |               |               |               |               |
+                        |     first     |    second     |     third     |123456789012345|
+                        |               |               |               |67890          |
+                        |               |               |               |               |
+                        |---------------|---------------|---------------|---------------|
+        """.trimIndent()
+        assertEquals(result, testRow)
+        log.info { "\n$testRow" }
 
-        log.info { "\n$text" }  // "\n" + text
+        val textFrames = Row("first", "second", "third", "12345678901234567890", width = 15, height = 5).apply {
+            rowBottom = true
+            rowTop = true
+            doubleFrames = true
+        }
+        val testRowFrames = textFrames.build()
+        val resultFrames = """
+                            |---------------||---------------||---------------||---------------|
+                            |               ||               ||               ||               |
+                            |               ||               ||               ||               |
+                            |     first     ||    second     ||     third     ||123456789012345|
+                            |               ||               ||               ||67890          |
+                            |               ||               ||               ||               |
+                            |---------------||---------------||---------------||---------------|
+        """.trimIndent()
+        assertEquals(resultFrames, testRowFrames)
+        log.info { "\n$testRowFrames" }
+
     }
 
     @Test
     fun buildTableTest() {
         val text = Table(
-            Row("hello world", "...", "test", width = 15, height = 5),
-            Row("world", "111111", "test222", width = 15, height = 5)
-        ).build()
+                Row("One", "TwoTwo", "Free", "FourFour", width = 15, height = 5),
+                Row("FiveFive", "SixSixSix", "SevenSevenSevenSevenSeven", "Eight", width = 15, height = 5),
+                Row("Nine", "TenTenTen", "Eleven", "Twelve", width = 15, height = 5)
+        ).apply { tableDoubleFrames = false }
+        val resultTable = """
+                            |---------------|---------------|---------------|---------------|
+                            |               |               |               |               |
+                            |               |               |               |               |
+                            |      One      |    TwoTwo     |     Free      |   FourFour    |
+                            |               |               |               |               |
+                            |               |               |               |               |
+                            |---------------|---------------|---------------|---------------|
+                            |               |               |               |               |
+                            |               |               |               |               |
+                            |   FiveFive    |   SixSixSix   |SevenSevenSeven|     Eight     |
+                            |               |               |SevenSeven     |               |
+                            |               |               |               |               |
+                            |---------------|---------------|---------------|---------------|
+                            |               |               |               |               |
+                            |               |               |               |               |
+                            |     Nine      |   TenTenTen   |    Eleven     |    Twelve     |
+                            |               |               |               |               |
+                            |               |               |               |               |
+                            |---------------|---------------|---------------|---------------|""".trimIndent()
+        val testTable = text.build()
+        assertEquals(resultTable, testTable)
+        log.info { "\n$testTable" }
 
-        log.info { "\n$text" }  // "\n" + text
+        val textAdjust = Table(
+                Row("One", "TwoTwo", "Free", "FourFour", width = 15, height = 5),
+                Row("FiveFive", "SixSixSix", "SevenSevenSevenSevenSeven", "Eight", width = 15, height = 5),
+                Row("Nine", "TenTenTen", "Eleven", "Twelve", width = 15, height = 5)
+        ).apply { tableDoubleFrames = false }
+        val resultTableAdjust = """
+                                    |---------|----------|--------------------------|---------|
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |   One   |  TwoTwo  |           Free           |FourFour |
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |---------|----------|--------------------------|---------|
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |FiveFive |SixSixSix |SevenSevenSevenSevenSeven |  Eight  |
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |---------|----------|--------------------------|---------|
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |  Nine   |TenTenTen |          Eleven          | Twelve  |
+                                    |         |          |                          |         |
+                                    |         |          |                          |         |
+                                    |---------|----------|--------------------------|---------|""".trimIndent()
+        textAdjust.adjustTable()
+        val testTableAdjust = textAdjust.build()
+        assertEquals(resultTableAdjust, testTableAdjust)
+        log.info { "\n$testTableAdjust" }
+
+        val textFrames = Table(
+                Row("One", "TwoTwo", "Free", "FourFour", width = 15, height = 5),
+                Row("FiveFive", "SixSixSix", "SevenSevenSevenSevenSeven", "Eight", width = 15, height = 5),
+                Row("Nine", "TenTenTen", "Eleven", "Twelve", width = 15, height = 5)
+        ).apply { tableDoubleFrames = true }
+        val resultTableFrames = """
+                                |---------------||---------------||---------------||---------------|
+                                |               ||               ||               ||               |
+                                |               ||               ||               ||               |
+                                |      One      ||    TwoTwo     ||     Free      ||   FourFour    |
+                                |               ||               ||               ||               |
+                                |               ||               ||               ||               |
+                                |---------------||---------------||---------------||---------------|
+                                |---------------||---------------||---------------||---------------|
+                                |               ||               ||               ||               |
+                                |               ||               ||               ||               |
+                                |   FiveFive    ||   SixSixSix   ||SevenSevenSeven||     Eight     |
+                                |               ||               ||SevenSeven     ||               |
+                                |               ||               ||               ||               |
+                                |---------------||---------------||---------------||---------------|
+                                |---------------||---------------||---------------||---------------|
+                                |               ||               ||               ||               |
+                                |               ||               ||               ||               |
+                                |     Nine      ||   TenTenTen   ||    Eleven     ||    Twelve     |
+                                |               ||               ||               ||               |
+                                |               ||               ||               ||               |
+                                |---------------||---------------||---------------||---------------|""".trimIndent()
+//        textFrames.adjustTable()
+        val testTableFrames = textFrames.build()
+        assertEquals(resultTableFrames, testTableFrames)
+        log.info { "\n$testTableFrames" }
     }
 }
