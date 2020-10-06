aa27bb5dfbd92ddbb4803e99d6e24f1f1d460df0
yevocama <alexandermacovey@gmail.com>
2020-10-06 20:19:39 +0300

    Sem04 Copy

--- amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Cell.kt
+++ amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Cell.kt
@@ -9,32 +9,13 @@ data class Cell(
     var width: Int,
     var height: Int,
 ) {
-    var left = false
-    var right = false
+    var left = true
+    var right = true
     var top = true
     var bottom = true
 
-    var centerAlign = true
-
     var verticalEdge = '-'
     var horizontalEdge = '|'
-    private var spaces = "%${width}s".format(" ")
-
-    private fun MutableList<String>.popFirstElement(): String{
-        return if (!isEmpty()){
-            val item = first()
-            remove(item)
-            item
-        }
-        else {
-            spaces
-        }
-    }
-
-    fun adjustCell(newWidth: Int?){
-        width = newWidth!!
-        spaces = "%${width}s".format(" ")
-    }
 
     /**
      *      width
@@ -45,21 +26,9 @@ data class Cell(
      *  |------------|
      */
     fun build(): String {
-//        val spaces = "%${width}s".format(" ")
+        val spaces = "%${width}s".format(" ")
         val horizontal = buildString { repeat(width) { append(verticalEdge) } }
-
-        val textIntoCell = mutableListOf<String>()
-        if (text.length >= width){
-            val substringIndices = if (text.length % width == 0) (text.length / width - 1)
-            else (text.length / width)
-            textIntoCell.add(text.stretch(width))
-            collect(substringIndices){
-                textIntoCell.add(text
-                    .substring(textIntoCell[0].length * (it + 1))
-                    .stretch(textIntoCell[0].length))
-            }
-        }
-        else textIntoCell.add(text)
+        val line = text.stretch(width)
 
         var totalHeight = height
         if (top) totalHeight++
@@ -67,21 +36,15 @@ data class Cell(
 
         val center = totalHeight / 2
 
+        // [0, 1, 2, 3 ... totalHeight - 1] -> ["|-------------|",  "|             |", ..... "....]
+        // it = 0 -> |-------------|
+        // it = 1 -> |             |
+        // .....
+
         return collect(totalHeight) {
             val internal = when (it) {
                 0 -> if (top) horizontal else spaces
-                in center..(center + textIntoCell.size) -> {
-                    val line = textIntoCell.popFirstElement()
-                    if (centerAlign){
-                        buildString {
-                            repeat((width - line.length) / 2 ) { append(" ")}
-                            append(line)
-                            repeat((width - line.length + 1) / 2 ) { append(" ")}
-                        }
-                    }
-                    else
-                        line.padEnd(width, ' ')
-                }
+                center -> line
                 totalHeight - 1 -> if (bottom) horizontal else spaces
                 else -> spaces
             }
--- amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Row.kt
+++ amcovey/task04/src/main/kotlin/ru/bmstu/iu4/sem04/table/Row.kt
@@ -2,14 +2,14 @@ package ru.bmstu.iu4.sem04.table
 
 data class Row(val cells: MutableList<Cell>) {
 
-    var rowBottom = false
-    var rowTop = false
-    var doubleFrames = false
-
-    /* constructor(vararg cells: Cell) : this(cells.toMutableList()) */
+    constructor(vararg cells: Cell) : this(cells.toMutableList())
 
     constructor(vararg texts: String, width: Int, height: Int) :
             this(texts.map { Cell(it, width, height) }.toMutableList())
+
+    fun adjustHeight() {
+
+    }
 
     /**
      *        0              1              2
@@ -20,25 +20,7 @@ data class Row(val cells: MutableList<Cell>) {
      *  |------------| |------------| |------------|
      */
     fun build(): String {
-        val tmp = cells.map {
-                when (it) {
-                    cells.first() -> it.apply {
-                        left = true
-                        right = doubleFrames
-                        bottom = rowBottom
-                        top = rowTop }.build().split("\n")
-                    cells.last() -> it.apply {
-                        right = true
-                        left = true
-                        bottom = rowBottom
-                        top = rowTop }.build().split("\n")
-                    else -> it.apply {
-                        left = true
-                        right = doubleFrames
-                        bottom = rowBottom
-                        top = rowTop }.build().split("\n")
-                }
-        }
+        val tmp = cells.map { it.build().split("\n") }
         val transpose = Array(tmp[0].size) { i -> Array(tmp.size) { j -> tmp[j][i] } }
         return transpose.joinToString("\n") { line -> line.joinToString("") }
     }
