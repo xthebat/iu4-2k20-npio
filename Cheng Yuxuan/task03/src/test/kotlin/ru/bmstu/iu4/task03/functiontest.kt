f893dac92996f93471e601ac249e08903406ab10
chengyuxuan <chengyuxuan1125@gmail.com>
2020-10-07 16:26:34 +0800

    Чэн Юйсюань.task03

--- Cheng Yuxuan/task03/src/test/kotlin/ru/bmstu/iu4/task03/functiontest.kt
+++ Cheng Yuxuan/task03/src/test/kotlin/ru/bmstu/iu4/task03/functiontest.kt
@@ -1,6 +1,10 @@
-package ru.bmstu.iu4.task03
+package ru.bmstu.iu4.task03.ru.bmstu.iu4.task03
 
 import org.junit.Test
+import ru.bmstu.iu4.task03.calcSumBalance
+import ru.bmstu.iu4.task03.convertString2Float
+import ru.bmstu.iu4.task03.filterByAge
+import ru.bmstu.iu4.task03.readDataFromJson
 import kotlin.test.assertEquals
 
 internal class FunctionsKtTest {
