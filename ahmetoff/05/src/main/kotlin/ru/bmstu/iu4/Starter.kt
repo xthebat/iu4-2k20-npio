9d8c6c607da9ac047aa24473a90cf2c477367ba1
akhmetov-dev <42318239+akhmetov-dev@users.noreply.github.com>
2020-10-08 12:49:42 +0300

    Задание 5
    Добавлены тесты

--- ahmetoff/05/src/main/kotlin/ru/bmstu/iu4/Starter.kt
+++ ahmetoff/05/src/main/kotlin/ru/bmstu/iu4/Starter.kt
@@ -11,21 +11,5 @@ object Starter {
     @JvmStatic
     fun main(args: Array<String>) {
 
-        val point = Point(1.0, 1.0).apply {
-            sym = '~'
-        }
-        val rectangle = Rectangle(Point(4.0, 3.0), Point(12.0, 6.0), 0).apply {
-            border = '$'
-        }
-
-        val circle = Circle(Point(10.0, 10.0), 5.0, 1).apply {
-            border = '|'
-        }
-
-        val list = listOf(rectangle, circle, point)
-
-        val plot = Canvas(20, 20, 2).draw(list)
-
-        plot.write()
     }
 }
