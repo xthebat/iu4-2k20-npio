97d432d27497e7eb726eec89eaa070368e0cee93
Andrey <korchagin.andrey.i@ya.ru>
2020-10-31 22:49:22 +0300

    Task03

---
+++ korchagin_a/task03/src/test/kotlin/ru/bmstu/iu4/korchagin/task03/JsonParserKtTest.kt
@@ -0,0 +1,10 @@
+package ru.bmstu.iu4.korchagin.task03
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class JsonParserKtTest {
+
+    @org.junit.jupiter.api.Test
+    fun getPriceToFloat() {
+    }
+}
